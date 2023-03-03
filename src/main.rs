cebe9ba9d5630dc012db54f567b03dca32acf5ed
HQ <hq7@hq7s-MacBook-Pro.local>
2023-03-03 17:52:15 -0500

    create Rust executive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+
+///this function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
